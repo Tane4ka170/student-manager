444eaa5d4d755c5103941ebc53521287738ba2a6
Tane4ka170 <tane4ka170@gmail.com>
2025-02-18 18:31:36 +0200

    Added authLogin action with API call

---
+++ frontend/src/store/auth/actions/authLogin.ts
@@ -0,0 +1,33 @@
+import BaseURL from "@/apis/BaseURL";
+import { axiosErrorHandler } from "@/utils";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+type TFormData = {
+  email: string;
+  password: string;
+};
+
+type TResponse = {
+  accessToken: string;
+  user: {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+  };
+};
+
+const authLogin = createAsyncThunk(
+  "auth/authLogin",
+  async (formData: TFormData, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const response = await BaseURL.post<TResponse>("/login", formData);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(axiosErrorHandler(error));
+    }
+  }
+);
+
+export default authLogin;
