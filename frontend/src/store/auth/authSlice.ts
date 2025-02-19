4edf8562bea2aef06fb9ca24ab2e3b45b6499e37
Tane4ka170 <tane4ka170@gmail.com>
2025-02-19 18:17:59 +0200

    feat: impl authSlice

--- frontend/src/store/auth/authSlice.ts
+++ frontend/src/store/auth/authSlice.ts
@@ -1,3 +1,39 @@
-interface IAuthState={
-    loading:TLoading
+import { TLoading } from "@/types/shared.types";
+import { createSlice } from "@reduxjs/toolkit";
+import authRegister from "./actions/authRegister";
+
+interface IUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
 }
+
+interface IAuthState {
+  loading: TLoading;
+  error: string | null;
+  accessToken: string | null;
+  user: IUser | null;
+}
+
+const initialState: IAuthState = {
+  loading: "idle",
+  error: null,
+  accessToken: null,
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(authRegister.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    });
+  },
+});
+
+export { authRegister, authLogin };
+export default authSlice.reducer;
