506e2359291696cc1d294995f2935e549034659f
Tane4ka170 <tane4ka170@gmail.com>
2025-02-19 18:45:14 +0200

    feat: add authSlice reducers

--- frontend/src/store/auth/authSlice.ts
+++ frontend/src/store/auth/authSlice.ts
@@ -1,6 +1,8 @@
 import { TLoading } from "@/types/shared.types";
 import { createSlice } from "@reduxjs/toolkit";
 import authRegister from "./actions/authRegister";
+import { isString } from "@/types";
+import authLogin from "./actions/authLogin";
 
 interface IUser {
   id: number;
@@ -26,11 +28,46 @@ const initialState: IAuthState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    cleanUI: (state) => {
+      state.loading = "idle";
+      state.error = "";
+    },
+    authLogout: (state) => {
+      state.accessToken = null;
+      state.user = null;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(authRegister.pending, (state) => {
       state.loading = "pending";
       state.error = null;
+    });
+    builder.addCase(authRegister.fulfilled, (state) => {
+      state.loading = "succeed";
+    });
+    builder.addCase(authRegister.rejected, (state, action) => {
+      state.loading = "failed";
+      if (isString(action.payload)) {
+        state.error = action.payload;
+      }
+    });
+
+    //   Login
+    builder.addCase(authLogin.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    });
+    builder.addCase(authLogin.fulfilled, (state, action) => {
+      state.loading = "succeed";
+      state.accessToken = action.payload.accessToken;
+      state.user = action.payload.user;
+    });
+    builder.addCase(authLogin.rejected, (state, action) => {
+      state.loading = "failed";
+      if (isString(action.payload)) {
+        state.error = action.payload;
+      }
     });
   },
 });
