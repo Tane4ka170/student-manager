43edec10aa0518bc9a6bbe4e59debc9039b2a208
Tane4ka170 <tane4ka170@gmail.com>
2025-02-19 18:18:44 +0200

    feat: add authReducer to store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -10,6 +10,7 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import authReducer from "./auth/authSlice";
 
 const authPersistConfig = {
   key: "auth",
