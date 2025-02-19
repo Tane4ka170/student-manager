a575b5508eac74bd2ed752406abe8abb3d432e00
Tane4ka170 <tane4ka170@gmail.com>
2025-02-19 18:45:39 +0200

    feat: add getStudents action

---
+++ frontend/src/store/students/actions/getStudents.ts
@@ -0,0 +1,29 @@
+import BaseURL from "@/apis/BaseURL";
+import { axiosErrorHandler } from "@/utils";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+type TResponse = TStudent[];
+
+const getStudents = createAsyncThunk(
+  "auth/getStudents",
+  async (_, thunkAPI) => {
+    const { rejectWithValue, fulfillWithValue, getState, signal } = thunkAPI;
+    const { auth } = getState() as RootState;
+    try {
+      const response = await BaseURL.get<TResponse>(
+        `/students?userId=${auth.user?.id}`,
+        { signal } // abort request
+      );
+
+      if (!response.data.length) {
+        return fulfillWithValue([]);
+      }
+
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(axiosErrorHandler(error));
+    }
+  }
+);
+
+export default getStudents;
