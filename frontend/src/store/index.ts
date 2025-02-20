f9090e52e5b68bdbe50593ce9c9042d04c6bc8cd
Tane4ka170 <tane4ka170@gmail.com>
2025-02-20 17:24:37 +0200

    feat: studentsSlice added to store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -11,6 +11,7 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import authReducer from "./auth/authSlice";
+import studentsReducer from "./students/studentsSlice";
 
 const authPersistConfig = {
   key: "auth",
