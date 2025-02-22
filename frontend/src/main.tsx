7721b9bc63cc06514b7949eb108047d0731dfe19
Tane4ka170 <tane4ka170@gmail.com>
2025-02-22 18:38:20 +0200

    feat: integrated AppRoute w/ main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
+import AppRoute from "./routes/AppRoute";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <PersistGate persistor={persistor}></PersistGate>
+    <PersistGate persistor={persistor}>
+      <AppRoute />
+    </PersistGate>
   </Provider>
 );
