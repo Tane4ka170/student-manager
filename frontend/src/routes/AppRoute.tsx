4f6a741a08ebd63d61603bdf69da3b4dde62a56b
Tane4ka170 <tane4ka170@gmail.com>
2025-02-22 18:01:53 +0200

    feat: added app routes

--- frontend/src/routes/AppRoute.tsx
+++ frontend/src/routes/AppRoute.tsx
@@ -1,15 +1,58 @@
 import App from "@/layouts/App";
-import { createBrowserRouter } from "react-router-dom";
+import {
+  AddStudent,
+  GetStudents,
+  Home,
+  Login,
+  Register,
+  UpdateStudent,
+} from "@/pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const route = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/students",
+        element: <GetStudents />,
+      },
+      {
+        path: "/students/add",
+        element: <AddStudent />,
+      },
+      {
+        path: "/students/edit/:id",
+        element: <UpdateStudent />,
+        loader: ({ params }) => {
+          if (!/^[0-9]$/i.test(params.id!)) {
+            throw new Response("Bad Request", {
+              statusText: `Students with id ${params.id} not found`,
+              status: 400,
+            });
+          }
+          return true;
+        },
+      },
+    ],
   },
 ]);
 
 const AppRoute = () => {
-  return <div>AppRoute</div>;
+  return <RouterProvider router={route} />;
 };
 
 export default AppRoute;
