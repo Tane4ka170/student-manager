7491ebf2646c736611a94822733597d32446672e
Tane4ka170 <tane4ka170@gmail.com>
2025-02-22 18:01:12 +0200

    feat: wraped app with Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}></PersistGate>
+  </Provider>
+);
