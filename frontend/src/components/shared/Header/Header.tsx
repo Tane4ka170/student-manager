bbad91d8c7528bf7d8c2e871725b4f21c77a8db1
Tane4ka170 <tane4ka170@gmail.com>
2025-02-23 16:10:29 +0200

    feat: added Navbar to Header

--- frontend/src/components/shared/Header/Header.tsx
+++ frontend/src/components/shared/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Container, Navbar as MainHeader } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import Logo from "@/assets/svg/Logo.svg";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
@@ -12,8 +13,13 @@ const Header = () => {
     >
       <Container>
         <MainHeader.Brand as={NavLink} to={"/"}>
-          <Logo />
+          <Logo width={"45px"} height={"45px"} />
+          <h1 className="fs-5 mb-0">Students Manager</h1>
         </MainHeader.Brand>
+        <MainHeader.Toggle aria-controls="basic-navbar-nav" />
+        <MainHeader.Collapse id="basic-navbar-nav">
+          <Navbar />
+        </MainHeader.Collapse>
       </Container>
     </MainHeader>
   );
