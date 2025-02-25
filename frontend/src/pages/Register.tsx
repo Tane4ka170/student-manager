3a9b358c8d53d125879123f4383a3818463ae2ad
Tane4ka170 <tane4ka170@gmail.com>
2025-02-25 17:55:55 +0200

    feat: added Register form w/ zod

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,30 @@
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import {
+  registerSchema,
+  TRegisterFormInputs,
+} from "@/validations/registerSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
+  const dispatch = useAppDispatch();
+  const { loading, error, accessToken } = useAppSelector(
+    (state) => state.students
+  );
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TRegisterFormInputs>({
+    mode: "onBlur",
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmitHandler: SubmitHandler<TRegisterFormInputs> = (data) => {
+    const { firstName, lastName, email } = data;
+  };
   return <div>Register</div>;
 };
 
