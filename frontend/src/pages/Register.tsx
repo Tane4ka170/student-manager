1cbcd933859a0f755799eb2c4454feeade62be38
Tane4ka170 <tane4ka170@gmail.com>
2025-02-25 19:17:50 +0200

    feat: added register submit logic

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,11 +1,14 @@
+import { authRegister, cleanUI } from "@/store/auth/authSlice";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import {
   registerSchema,
   TRegisterFormInputs,
 } from "@/validations/registerSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Register = () => {
   const dispatch = useAppDispatch();
@@ -23,9 +26,33 @@ const Register = () => {
   });
 
   const onSubmitHandler: SubmitHandler<TRegisterFormInputs> = (data) => {
-    const { firstName, lastName, email } = data;
+    const { firstName, lastName, email, password } = data;
+    dispatch(authRegister({ firstName, lastName, email, password }))
+      .unwrap()
+      .then(() => navigate("/login?message=registered"));
   };
-  return <div>Register</div>;
+
+  useEffect(() => {
+    return () => {
+      dispatch(cleanUI());
+    };
+  }, [dispatch]);
+
+  if (accessToken) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <section className="py-5">
+      <Container>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form noValidate onSubmit={handleSubmit(onSubmitHandler)}></Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Register;
