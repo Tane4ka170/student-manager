44619aceb410090133ca4d0b1f147e16a2060916
Tane4ka170 <tane4ka170@gmail.com>
2025-02-26 18:51:00 +0200

    refactor: App w/ shared imports

--- frontend/src/layouts/App.tsx
+++ frontend/src/layouts/App.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import styles from "./styles.module.css";
-import Header from "@/components/shared/Header/Header";
-import Footer from "@/components/shared/Footer/Footer";
+import { Footer, Header } from "@/components/shared";
 
 const App = () => {
   return (
