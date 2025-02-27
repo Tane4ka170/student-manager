b94c2df2ff50269c24eea458668ed1b49ba6fee6
Tane4ka170 <tane4ka170@gmail.com>
2025-02-27 16:31:36 +0200

    feat: impl GetStudents page

--- frontend/src/pages/GetStudents.tsx
+++ frontend/src/pages/GetStudents.tsx
@@ -1,5 +1,51 @@
+import { Heading } from "@/components/shared";
+import StudentsList from "@/components/studentsManager/StudentsList/StudentsList";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { getStudents, studentsCleanUP } from "@/store/students/studentsSlice";
+import { useEffect, useState } from "react";
+import { Alert, Container } from "react-bootstrap";
+
 const GetStudents = () => {
-  return <div>GetStudents</div>;
+  const dispatch = useAppDispatch();
+  const {
+    get: { loading, error },
+    records,
+    message,
+  } = useAppSelector((state) => state.students);
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    const promise = dispatch(getStudents());
+    return () => {
+      dispatch(studentsCleanUP());
+      promise.abort();
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    const timeId = setTimeout(() => setShow(false), 3000);
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, []);
+
+  return (
+    <section className="py-4">
+      <Container>
+        <Heading title="Students List" />
+        {message && message === "added" && (
+          <Alert variant="success">Student Successfully Added</Alert>
+        )}
+        {message && message === "edited" && (
+          <Alert variant="success">Student Successfully Updated</Alert>
+        )}
+        {message && message === "already_exist" && (
+          <Alert variant="danger">This student is already in the system!</Alert>
+        )}
+        {records.length ? <StudentsList students={records} /> : <p></p>}
+      </Container>
+    </section>
+  );
 };
 
 export default GetStudents;
