9646147b945cb3649677ec08c9a8d4cc4abaa353
Tane4ka170 <tane4ka170@gmail.com>
2025-02-28 17:11:25 +0200

    feat: StudentsList w/ Student items

--- frontend/src/components/studentsManager/StudentsList/StudentsList.tsx
+++ frontend/src/components/studentsManager/StudentsList/StudentsList.tsx
@@ -1,8 +1,26 @@
 import { TStudent } from "@/types";
+import { Table } from "react-bootstrap";
+import Student from "../Student/Student";
 
 const StudentsList = ({ students }: { students: TStudent[] }) => {
-  const studentsList = students.map();
-  return <div>StudentsList</div>;
+  const studentsList = students.map((student) => {
+    return <Student key={student.id} {...student} />;
+  });
+  return (
+    <Table striped bordered hover responsive>
+      <thead>
+        <tr>
+          <th>Student id</th>
+          <th>Fist Name</th>
+          <th>Last Name</th>
+          <th>Reference</th>
+          <th>Email</th>
+          <th>Actions</th>
+        </tr>
+        <tbody>{studentsList}</tbody>
+      </thead>
+    </Table>
+  );
 };
 
 export default StudentsList;
