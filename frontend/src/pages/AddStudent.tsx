c5b30470baa580f1140d0fd3951f63904b2d56b8
Tane4ka170 <tane4ka170@gmail.com>
2025-02-28 17:11:58 +0200

    feat: created AddStudent layout

--- frontend/src/pages/AddStudent.tsx
+++ frontend/src/pages/AddStudent.tsx
@@ -1,4 +1,12 @@
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { useNavigate } from "react-router-dom";
+
 const AddStudent = () => {
+  const dispatch = useAppDispatch();
+  const {
+    add: { loading, error },
+  } = useAppSelector((store) => store.students);
+  const navigate = useNavigate();
   return <div>AddStudent</div>;
 };
 
